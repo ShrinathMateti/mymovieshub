011c18ab5e688f5189f569861a3ddc703f92b6e2
ShrinathMateti <shrinathrmateti@gmail.com>
2025-09-13 08:54:49 +0530

    small update

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,7 +3,7 @@ const SearchBox = ({ value, setSearchValue }) => {
     <div className="w-full md:w-1/3 flex justify-center md:justify-end">
       <input
         type="text"
-        className="w-11/12 sm:w-5/6 md:w-full rounded-lg px-4 py-2 bg-slate-800 text-white placeholder-gray-400 
+        className="w-11/12 sm:w-5/6 md:w-full rounded-lg px-5 py-2 bg-slate-800 text-white placeholder-gray-400 
                    focus:outline-none focus:ring-2 focus:ring-yellow-400"
         value={value}
         onChange={(event) => setSearchValue(event.target.value)}
